114ecb40d537d23de8132f778d8378f802a2d9e4
annalightmagic <116140836+annalightmagic@users.noreply.github.com>
2022-11-30 12:08:40 +0700

    Add files via upload

---
+++ footballteamforloop.py
@@ -0,0 +1,17 @@
+football_team = []
+
+for i in range(11):
+    name = input("Add a player in the football team: ")
+    football_team.append(name)
+print(football_team)
+
+repeat = 'Y'
+while repeat == 'Y':
+    edit = int(input("Which player do you want to change?: "))
+    football_team[edit-1] = input("Enter a new player: ")
+
+    delete = int(input("Which player do you want to delete?: "))
+    del football_team[delete-1]
+
+    repeat = input('Do you want to edit and delete name(Y/N)')
+print(football_team)
