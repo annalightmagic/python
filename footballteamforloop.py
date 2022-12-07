8243fe81a413ded800cf03d00a6628af4ca7a083
annalightmagic <116140836+annalightmagic@users.noreply.github.com>
2022-12-07 11:43:51 +0700

    Add files via upload

--- footballteamforloop.py
+++ footballteamforloop.py
@@ -7,11 +7,21 @@ print(football_team)
 
 repeat = 'Y'
 while repeat == 'Y':
-    edit = int(input("Which player do you want to change?: "))
-    football_team[edit-1] = input("Enter a new player: ")
+    print(football_team)
+    final_index = len(football_team)
+    print("You can print any name from the list. Index numbers go from 0 t0", final_index)
+    i = int(input("Which name do you want to change?: "))
+    while i >= len(football_team):
+        i = int(input("Enter correct number: "))
+    football_team[i] = int(input("Enter a new name: "))
+    print(football_team)
 
-    delete = int(input("Which player do you want to delete?: "))
-    del football_team[delete-1]
+    print("You can print any name from the list. Index numbers go from 0 t0", final_index)
+    i = int(input("Which name do you want to delete?: "))
+    while i >= len(football_team):
+        i = int(input("Enter correct number: "))
+    del football_team[i]
+    print(football_team)
+    repeat = input("Do you want to edit or delete name? (Y/N)")
 
-    repeat = input('Do you want to edit and delete name(Y/N)')
-print(football_team)
+
