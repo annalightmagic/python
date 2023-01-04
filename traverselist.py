b9ab7f2348159363e50be046d74f90f5e4748ba7
annalightmagic <116140836+annalightmagic@users.noreply.github.com>
2023-01-04 12:05:31 +0700

    Add files via upload

---
+++ traverselist.py
@@ -0,0 +1,8 @@
+teamlist = []
+repeat = "Y"
+while repeat == "Y":
+    name = input("Enter a name: ")  
+    teamlist.append(name)
+    repeat = input("Do you want to add another name?(Y/N) ")
+for i in range(len(teamlist)):
+    print(i, teamlist[i])
